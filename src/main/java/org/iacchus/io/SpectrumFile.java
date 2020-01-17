12dc41c6f0f4c42529d6366453c60c6285bea307
JanBloch <jan@bhome.ch>
2020-01-17 15:28:13 +0100

    added constant HEADER_LENGTH to class SpectrumFile.java

--- src/main/java/org/iacchus/io/SpectrumFile.java
+++ src/main/java/org/iacchus/io/SpectrumFile.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class SpectrumFile {
+    public static final int HEADER_LENGTH = 7;
     byte[] bytes;
     public SpectrumFile(File f) throws IOException {
         bytes = Files.readAllBytes(f.toPath());
@@ -16,14 +17,15 @@ public class SpectrumFile {
     }
 
     public int[] getFrequencies(int time){
-        int startIndex = (getRowLength()*4)*time+7;
-        int endIndex = (getRowLength()*4)*(time+1)+7-1;
+        int startIndex = (getRowLength()*4)*time+HEADER_LENGTH;
+        int endIndex = (getRowLength()*4)*(time+1)+HEADER_LENGTH-1;
         int[] out = new int[endIndex-startIndex];
         for(int i=startIndex;i<endIndex;i++){
             out[i-startIndex] = bytes[i];
         }
         return out;
     }
+    
     public int getRowLength(){
         String binaryString = "";
         for(int i=4; i<8; i++){
