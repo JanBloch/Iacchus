efbf9a9dee4ccb491ab7c17726bb010a02b874c2
JanBloch <jan@bhome.ch>
2020-01-17 15:09:50 +0100

    added Class SpectrumFile.java added function to get frequencies at specific time added function to get row length of file

--- src/main/java/org/iacchus/io/SpectrumFile.java
+++ src/main/java/org/iacchus/io/SpectrumFile.java
@@ -1,5 +1,34 @@
 package org.iacchus.io;
 
+import org.iacchus.audio.Processor;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class SpectrumFile {
-    
+    byte[] bytes;
+    public SpectrumFile(File f) throws IOException {
+        bytes = Files.readAllBytes(f.toPath());
+    }
+    public SpectrumFile(String path) throws IOException {
+        bytes = Files.readAllBytes(new File(path).toPath());
+    }
+
+    public int[] getFrequencies(int time){
+        int startIndex = (getRowLength()*4)*time+7;
+        int endIndex = (getRowLength()*4)*(time+1)+7-1;
+        int[] out = new int[endIndex-startIndex];
+        for(int i=startIndex;i<endIndex;i++){
+            out[i-startIndex] = bytes[i];
+        }
+        return out;
+    }
+    public int getRowLength(){
+        String binaryString = "";
+        for(int i=4; i<8; i++){
+            binaryString = Processor.byteToString(bytes[i]) + binaryString;
+        }
+        return Integer.parseInt(binaryString, 2);
+    }
 }
