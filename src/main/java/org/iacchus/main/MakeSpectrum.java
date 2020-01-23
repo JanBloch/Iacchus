b3de64b19b822ec6791e9fff594cbedb7f6999d2
JanBloch <jan@bhome.ch>
2020-01-23 16:50:54 +0100

    Forgot what I did but it works

--- src/main/java/org/iacchus/main/MakeSpectrum.java
+++ src/main/java/org/iacchus/main/MakeSpectrum.java
@@ -2,20 +2,29 @@ package org.iacchus.main;
 
 import org.iacchus.audio.Processor;
 
+import java.util.Arrays;
+
 public class MakeSpectrum {
     public static int maxFreqPower = 13;
     public static int maxFreq = (int)Math.pow(2, maxFreqPower);
     public static void main(String[] args) {
-
         String outFile;
         String inFile;
         Processor p = new Processor();
         try {
-            if(args[0].equals("-o")){
-                outFile = args[1];
-                inFile = args[2];
+            int indexOut = -1;
+            int indexPower = -1;
+            indexOut = Arrays.asList(args).indexOf("-o");
+            indexPower = Arrays.asList(args).indexOf("-p");
+            if(indexPower != -1){
+                maxFreqPower = Integer.parseInt(args[indexPower + 1]);
+                maxFreq = (int)Math.pow(2, maxFreqPower);
+            }
+            if(indexOut != -1){
+                outFile = args[indexOut+1];
+                inFile = args[args.length-1];
             }else{
-                inFile = args[0];
+                inFile = args[args.length-1];
                 outFile = inFile + ".spectrum";
             }
             double[] audio = p.getAudio(inFile);
